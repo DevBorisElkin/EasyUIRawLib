82adeac330795ef565a8616275e58fe65bc5cd55
DevBorisElkin <ea.foreverrr@gmail.com>
2022-12-04 23:54:56 +0300

    backup somewhat working

--- EasyUIRawLib/ReadyUIElements/FadedScrollView/FadedScrollView.swift
+++ EasyUIRawLib/ReadyUIElements/FadedScrollView/FadedScrollView.swift
@@ -8,6 +8,8 @@
 import UIKit
 
 class FadedScrollView: UIScrollView {
+    @IBInspectable var isVertical: Bool = true
+    
     @IBInspectable var enableTopFade: Bool = false
     @IBInspectable var enableBottomFade: Bool = false
     
@@ -17,6 +19,13 @@ class FadedScrollView: UIScrollView {
     // internal items
     
     var layerGradient: CAGradientLayer?
+    
+    private let transparentColor = UIColor.white.withAlphaComponent(0)
+    private lazy var cgTransparentColor = transparentColor.cgColor
+    private let opaqueColor = UIColor.white
+    private lazy var cgOpaqueColor = opaqueColor.cgColor
+    
+    var progressManager: ProgressManager!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -29,6 +38,7 @@ class FadedScrollView: UIScrollView {
             CATransaction.setDisableActions(true)
             layerGradient?.frame = safeAreaLayoutGuide.layoutFrame
             CATransaction.commit()
+            manageGradientColorsOnScroll()
         }
     }
     
@@ -38,18 +48,16 @@ class FadedScrollView: UIScrollView {
     }
     
     private func commonInit() {
+        progressManager = isVertical == true ? VerticalProgressManager() : HorizontalProgressManager()
+        progressManager.configure(parentScrollView: self, startFadeSizeMult: topFadeSizeMult, endFadeSizeMult: bottomFadeSizeMult)
         configureFadeLayer()
     }
     
     private func configureFadeLayer() {
-        
-        let transparentColor = UIColor.white.withAlphaComponent(0).cgColor
-        let opaqueColor = UIColor.white.cgColor
-        
         let layerGradient = CAGradientLayer()
         self.layerGradient = layerGradient
         
-        layerGradient.colors = [transparentColor, opaqueColor, opaqueColor, transparentColor]
+        layerGradient.colors = [cgTransparentColor, cgOpaqueColor, cgOpaqueColor, cgTransparentColor]
         
         layerGradient.frame = bounds
         layerGradient.startPoint = CGPoint(x: 0, y: 0)
@@ -65,4 +73,88 @@ class FadedScrollView: UIScrollView {
         super.layoutSubviews()
     }
     
+    // MARK: Mange gradient colors on scroll
+    // todo
+    private func manageGradientColorsOnScroll() {
+        
+        if var colors = layerGradient?.colors as? [CGColor] {
+            let progress = progressManager.calculateProgress()
+            let startAbsProgress = progressManager.calculateStartAlpha(progress: progress)
+            let endAbsProgress = progressManager.calculateEndAlpha(progress: progress)
+            
+            print("progress: \(progress)")
+            print("startAbsProgress: \(startAbsProgress), endAbsProgress: \(endAbsProgress)")
+            
+            
+            colors[0] = opaqueColor.withAlphaComponent(startAbsProgress).cgColor
+            colors[3] = opaqueColor.withAlphaComponent(endAbsProgress).cgColor
+            
+            layerGradient?.colors = colors
+        }
+        
+//        var finalColors: [CGColor] = [opaqueColor, opaqueColor]
+//        if enableTopFade {
+//            finalColors.insert(transparentColor, at: 0)
+//        }
+//
+//        if enableBottomFade {
+//
+//        }
+    }
+    
+    private func calculateProgress() -> CGFloat {
+        return contentOffset.y / contentSize.height
+    }
+    
+    private func calculateAlphaFromProgress() {
+        
+    }
+    
+    class ProgressManager {
+        weak var parentScrollView: UIScrollView?
+        var startFadeSizeMult: CGFloat = 0
+        var endFadeSizeMult: CGFloat = 0 // 0.1 of scroll view height
+        
+        func calculateProgress() -> CGFloat { 0 }
+        func calculateStartAlpha(progress: CGFloat) -> CGFloat { 0 }
+        func calculateEndAlpha(progress: CGFloat) -> CGFloat { 0 }
+        
+        func configure(parentScrollView: UIScrollView?, startFadeSizeMult: CGFloat, endFadeSizeMult: CGFloat) {
+            self.parentScrollView = parentScrollView
+            self.startFadeSizeMult = startFadeSizeMult
+            self.endFadeSizeMult = endFadeSizeMult
+        }
+    }
+    
+    class VerticalProgressManager: ProgressManager {
+        override func calculateProgress() -> CGFloat {
+            guard let parentScrollView = parentScrollView else { return 0 }
+            let contentHeight = parentScrollView.contentSize.height
+            let contentOffset = parentScrollView.contentOffset.y
+            
+            let maxHeightForProgress = contentHeight - parentScrollView.bounds.height
+            return max(min(contentOffset / maxHeightForProgress, 1), 0)
+        }
+        
+        override func calculateStartAlpha(progress: CGFloat) -> CGFloat {
+            if progress > startFadeSizeMult { return 0 }
+            return CGFloat(1) - progress / startFadeSizeMult
+        }
+        override func calculateEndAlpha(progress: CGFloat) -> CGFloat {
+            let progressMargin = CGFloat(1) - endFadeSizeMult // 0.9
+            print("realSndProgress: \(progress - progressMargin)")
+            if progress < progressMargin { return 0 }
+            
+            return (progress - progressMargin) / endFadeSizeMult
+        }
+    }
+    
+    class HorizontalProgressManager: ProgressManager {
+        override func calculateProgress() -> CGFloat {
+            guard let parentScrollView = parentScrollView else { return 0 }
+            let contentWidth = parentScrollView.contentSize.width
+            let contentOffset = parentScrollView.contentOffset.x
+            return (contentOffset + parentScrollView.bounds.width) / contentWidth
+        }
+    }
 }
