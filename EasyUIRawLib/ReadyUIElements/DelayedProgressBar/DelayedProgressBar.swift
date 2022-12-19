b69bd770ef4511795770f5b1c4a0a591a3428fc4
DevBorisElkin <ea.foreverrr@gmail.com>
2022-12-20 01:16:46 +0300

    backup creating delayed progress bar

---
+++ EasyUIRawLib/ReadyUIElements/DelayedProgressBar/DelayedProgressBar.swift
@@ -0,0 +1,71 @@
+//
+//  DelayedProgressBar.swift
+//  EasyUIRawLib
+//
+//  Created by test on 20.12.2022.
+//
+
+import UIKit
+
+// it's horizontal only
+class DelayedProgressBar: UIView {
+    
+    private var instantProgressView: UIView!
+    private var instantProgressViewWidthConstraint: NSLayoutConstraint!
+    private var delayedProgressView: UIView!
+    private var delayedProgressViewWidthConstraint: NSLayoutConstraint!
+    
+    private var selectedColorSet: ColorSet!
+    private var progressAnimationTime: Double!
+    
+    // todo add ability to set up via nib
+    override func awakeFromNib() {}
+    
+    func configure(colorSet: ColorSet, progressAnimationTime: Double) {
+        selectedColorSet = colorSet
+        self.progressAnimationTime = progressAnimationTime
+        commonInit()
+    }
+    
+    private func commonInit() {
+        let instantProgressViewTuple = createAndConstraintProgressView()
+        instantProgressView = instantProgressViewTuple.view
+        instantProgressViewWidthConstraint = instantProgressViewTuple.widthConstraint
+        instantProgressView.backgroundColor = selectedColorSet.instantProgressColor
+        
+        let delayedProgressViewTuple = createAndConstraintProgressView()
+        delayedProgressView = delayedProgressViewTuple.view
+        delayedProgressViewWidthConstraint = delayedProgressViewTuple.widthConstraint
+        delayedProgressView.backgroundColor = selectedColorSet.delayedProgressColor
+    }
+    
+    func showProgress(previousProgress: Double, currentProgress: Double) {
+        checkProgress(previousProgress)
+        checkProgress(currentProgress)
+        instantProgressViewWidthConstraint.multiplier = CGFloat(currentProgress)
+    }
+    
+    private func checkProgress(_ progress: Double) {
+        guard progress >= 0 && progress <= 1  else { return }
+        fatalError("Error! Progress should be within 0...1 range!")
+    }
+    
+    private func createAndConstraintProgressView() -> (view: UIView, widthConstraint: NSLayoutConstraint) {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(view)
+        view.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        view.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        view.leadingAnchor.constraint(equalTo: leadingAnchor).isActive = true
+        let widthAnchor = view.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0)
+        widthAnchor.isActive = true
+        return (view, widthAnchor)
+    }
+    
+    
+    struct ColorSet {
+        var backgroundColor: UIColor
+        var instantProgressColor: UIColor
+        var delayedProgressColor: UIColor
+    }
+}
---
+++ EasyUIRawLib/Thoughts and ideas.swift
@@ -0,0 +1,46 @@
+//
+//  Thoughts and ideas.swift
+//  EasyUIRawLib
+//
+//  Created by test on 19.12.2022.
+//
+
+/*
+ 
+ Elements:
+ 1) Faded scroll views
+ 2) Tap Animated views and buttons
+ 3) bottom configurable down menu
+ 4) progress bar and delayed progress bar
+ 5) top menu that shows for a limited amount of time and then closed
+ 
+ 
+ todo:
+ 1) Fix linear/logarithmic interpolation setup in code (simplify with enums)
+ 2) Make all scroll-dependant elements a subject to faded scroll
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ */
+
